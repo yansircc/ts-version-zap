2a0560f22ec95a2a4e86e57969e0eeec91f7cae9
yansir <markyan@foxmail.com>
2024-03-31 04:27:49 +0800

    Update static file path in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 // .src/server.ts
 import express from 'express';
-import { logger } from './utils/logger';
+import path from 'path';
+
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('src/public'));
+app.use(express.static(path.join(__dirname, 'services/public')));
 
 export const server = app.listen(port, () => {
     //logger.info(`服务器已在 http://localhost:${port} 启动。`);
